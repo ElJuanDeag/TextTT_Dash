5897ba1d850df1d8559a606b2479612b057f9f2b
Brajesh Kumar <47526257+ElJuanDeag@users.noreply.github.com>
2023-04-16 11:27:00 +0100

    Added current working version on Git
    This version is the one designed to work with Looker studio and constraints of Google sheets.
    This one is only capable at showing monthwise breakdown.

---
+++ fetchAndSave_Daily.py
@@ -0,0 +1,50 @@
+import os
+import datetime as dt
+from datetime import datetime
+from datetime import timedelta
+import datetime as dt
+import requests
+import json
+from requests_negotiate_sspi import HttpNegotiateAuth
+import pandas as pd
+import numpy as np
+import re
+import glob
+
+filelocation=r'C:\\Python\\TextTT_Dash\\fills_folder\\'
+
+def fetchData(date):
+    requestURL = "http://hgdataapi/TT/FillsAudit"
+    fromDate=date
+    toDate=date    #From and to date must be same for only one day can be supplied by API
+    
+
+    r = requests.get('{url}?from={fromDate}&to={toDate}'.format(url=requestURL, fromDate=fromDate, toDate=toDate),auth=HttpNegotiateAuth())
+    if r.status_code == 200: 
+        try:    
+            jsonResponse = json.loads(r.text.encode('ascii', 'ignore'))
+            df=pd.DataFrame(jsonResponse)
+        except: 
+            print()
+    else:
+        print('Request failed:{0}, Reason: {1}'.format(r.status_code, r.reason))
+        jsonResponse = []
+        df=pd.DataFrame([])
+        #Removes all ASE rows leaving only quoted legs 
+    
+    if os.path.isfile(filelocation+'fills_of_date_'+toDate+'.csv'):
+        os.remove(filelocation+'fills_of_date_'+toDate+'.csv')
+    df.to_csv(filelocation+'fills_of_date_'+toDate+'.csv')
+
+def fillsInRange(startDate,endDate):
+    bday_pd = pd.offsets.CustomBusinessDay( weekmask='Mon Tue Wed Thu Fri Sun')
+    rangeDate=pd.date_range(start=startDate,end=endDate,freq=bday_pd).date.astype(str)
+    results = []
+    for date in rangeDate:
+        fetchData(date)
+        print('fetched for:'+date)
+
+today=dt.datetime.today()
+yesterday = today - timedelta(days = 1)
+if not today.weekday()==5:
+    fetchData(str('{:02d}'.format(today.year))+'-'+str('{:02d}'.format(today.month))+'-'+str('{:02d}'.format(today.day)))
---
+++ processPivot.py
@@ -0,0 +1,70 @@
+import os
+import datetime as dt
+from datetime import datetime
+import requests
+import json
+from requests_negotiate_sspi import HttpNegotiateAuth
+import pandas as pd
+import numpy as np
+import re
+import glob
+import tqdm
+
+filelocation='C:\\Python\\TextTT_Dash\\fills_folder\\'
+
+csv_files = glob.glob(filelocation+'fills_of_date_'+'*.csv')
+df_original=pd.DataFrame()
+for file in csv_files:
+    df_temp = pd.read_csv(file)
+    df_original= df_original.append(df_temp, ignore_index=True)
+
+df=pd.DataFrame()
+df=df_original
+df.drop(['Unnamed: 0'],axis=1,inplace=True)
+df['tradeDate']=pd.to_datetime(df['tradeDate'])
+df['year_month']=df['tradeDate'].dt.year.astype(str)+'/'+df['tradeDate'].dt.month.astype(str)
+df=df[df['securityType']=='FUTURE']
+df.textTT.fillna('MANUAL',inplace=True)
+df['textTT']=df['textTT'].apply(lambda x: re.search("(?<=:)[^:]*$", x).group() if ':' in x else x).str.strip()
+df.to_csv(filelocation+'rawFills.csv')
+
+def transform_row(row):
+    TextTT_dict={"rangetraderprofit":"RangeTrader",
+         "Rangeentry2":"RangeTrader",
+          "rangetraderentry":"RangeTrader",
+         "RT_ASE_Entry":"RangeTrader_Gen2",
+         "RT_ASE_Exit":"RangeTrader_Gen2",
+          "RRT_ASE_Entry":"RangeTrader_Gen2",
+          "RRT_ASE_Exit":"RangeTrader_Gen2",
+          "PRT_LimitEntry":"RangeTrader_Gen2",
+          "PRT_Exit":"RangeTrader_Gen2",
+          "PRT_ReEntry":"RangeTrader_Gen2",
+          "PRT_FixedExit":"RangeTrader_Gen2",
+          "PRT_ASE_Limit":"RangeTrader_Gen2",
+          "PRT_ASE_Exit":"RangeTrader_Gen2",
+         }
+    if pd.isna(row['algoName']):#=='':
+        if row['textTT'] in TextTT_dict:
+            if row['algo']=='ASE':
+                return 'ASE & '+TextTT_dict[row['textTT']]
+            else:
+                return TextTT_dict[row['textTT']]
+        elif row['textTT'] == 'MANUAL':
+            if row['algo'] == 'ASE':
+                return 'ASE'
+            else:
+                return 'Manual'
+        else:
+            return 'Pvt_Algo'
+    else:
+        return row['algoName']
+
+df['algoName'] = df.apply(lambda row: transform_row(row),axis=1)
+df['algofill']=np.where(df['algo']=='Manual',0, df['fillQty'])
+df['algofill']=np.where(df['algo']=='Manual',0, df['fillQty'])
+pivot_table=df.pivot_table(index=['year_month','itm','ttProductCode', 'algo','algoName'], values=['algofill','fillQty'], aggfunc='sum').reset_index()
+
+
+if os.path.isfile(filelocation+'pivot.csv'):
+    os.remove(filelocation+'pivot.csv')
+pivot_table.to_csv(filelocation+'pivot.csv',index=False)
