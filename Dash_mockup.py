d1e91344291d395c6325df3223379e499e2b9b94
m0squoto <brajeshagent47@gmail.com>
2023-04-28 21:52:55 +0100

    Created csv list for storing product class and batch list for users.

--- Dash_mockup.py
+++ Dash_mockup.py
@@ -30,7 +30,7 @@ st.set_page_config(layout='wide', initial_sidebar_state='expanded')
 logo=Image.open("C:/Python/Git/assets/ff_logo.png")
 st.sidebar.image(logo)
 
-with open('style.css') as f:
+with open('C:/Python/Git/style.css') as f:
     st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
     
 st.sidebar.header('Algo% Dashboard `mockup`')
